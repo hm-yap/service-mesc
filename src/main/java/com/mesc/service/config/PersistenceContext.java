d27159b445cc03943b7d33fac025c07611b12e57
Hao Meng Yap <haomengyap@gmail.com>
2015-11-14 15:43:17 +0800

    Added persistence context Removed Spring MVC from pom Updated to mysql in dbconfig

---
+++ src/main/java/com/mesc/service/config/PersistenceContext.java
@@ -0,0 +1,29 @@
+package com.mesc.service.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class PersistenceContext {
+	/**
+	 * Creates and configures Hikari data source
+	 * @param env
+	 * @return
+	 */
+	@Bean(destroyMethod = "close")
+    DataSource dataSource(Environment env) {
+        HikariConfig dataSourceConfig = new HikariConfig();
+        dataSourceConfig.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSourceConfig.setJdbcUrl(env.getRequiredProperty("db.url"));
+        dataSourceConfig.setUsername(env.getRequiredProperty("db.username"));
+        dataSourceConfig.setPassword(env.getRequiredProperty("db.password"));
+ 
+        return new HikariDataSource(dataSourceConfig);
+    }
+}
