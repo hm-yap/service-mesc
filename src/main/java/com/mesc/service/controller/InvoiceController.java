bd8757712e15ae9647ad0fb8e4ad7ec1967c6cc8
Hao Meng Yap <haomengyap@gmail.com>
2015-11-22 17:17:51 +0800

    Update to make use of GsonUtil

--- src/main/java/com/mesc/service/controller/InvoiceController.java
+++ src/main/java/com/mesc/service/controller/InvoiceController.java
@@ -9,18 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.mesc.service.entity.Invoice;
 import com.mesc.service.entity.Tax;
 import com.mesc.service.services.InvoiceService;
+import com.mesc.service.util.GsonUtil;
 
 @RestController
 public class InvoiceController {
 	/* Path for Invoice related services */
 	private static final String INVOICE_PATH = "/invoice";
 
-	private static Gson gson = new GsonBuilder().create();
 	private final InvoiceService invService;
 
 	@Autowired
@@ -35,7 +33,7 @@ public class InvoiceController {
 	 */
 	@RequestMapping(value = INVOICE_PATH, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String createInvoice() {
-		return gson.toJson(invService.create(), Invoice.class);
+		return GsonUtil.objToJson(invService.create(), Invoice.class);
 	}
 
 	/**
@@ -45,7 +43,7 @@ public class InvoiceController {
 	 */
 	@RequestMapping(value = INVOICE_PATH, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String invoicePreview() {
-		return gson.toJson(new Invoice(), Invoice.class);
+		return GsonUtil.objToJson(new Invoice(), Invoice.class);
 	}
 
 	/**
@@ -55,7 +53,7 @@ public class InvoiceController {
 	 */
 	@RequestMapping(value = INVOICE_PATH, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getLatestInvoice() {
-		return gson.toJson(new Invoice(), Invoice.class);
+		return GsonUtil.objToJson(new Invoice(), Invoice.class);
 	}
 
 	/**
@@ -73,6 +71,6 @@ public class InvoiceController {
 		BigDecimal invAmt = new BigDecimal(orderId);
 		invAmt = invAmt.divide(new BigDecimal("100"));
 		BigDecimal taxPct = new BigDecimal("0.06");
-		return gson.toJson(new Tax(invAmt, taxPct), Tax.class);
+		return GsonUtil.objToJson(new Tax(invAmt, taxPct), Tax.class);
 	}
 }
